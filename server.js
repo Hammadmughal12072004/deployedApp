e0a77112601c62083f27be0e39414572250127d3
hammadmughal12072004 <hammadanwaar04@gmail.com>
2024-06-11 14:14:03 +0500

    New

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use('/tutorials', tutorialRoute);
 
 app.use(express.static(path.join(__dirname, './client/build')));
 
-app.get('*', (res, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
